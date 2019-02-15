325281c4306cac6098f903fdd008370bef8ad8a9
Tilen Prevolnik <tilen.prevolnik@gigodesign.com>
2019-02-15 17:42:25 +0100

    add task - change placeholder to default text

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $('.input-box--submit').on('click', function () {
     if (!inputFieldValue) {
         inputField.addClass('empty-field').attr('placeholder', 'Please enter a new task');
     } else {
-        inputField.removeClass('empty-field').val('');
+        inputField.removeClass('empty-field').val('').attr('placeholder', 'Enter new task');
         $('.task-list').append(listItem);
     }
 });
