f6a21f027e7b05e6c46bda81d6bab1aa31740502
Tilen Prevolnik <tilen.prevolnik@gigodesign.com>
2019-02-15 17:05:07 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$('.input-box--submit').on('click', function () {
+    let inputField = $('.input-box--input');
+    let inputFieldValue = inputField.val();
+    let listItem = '<li class="task-list--item">' + inputFieldValue + '</li>';
+
+    if (!inputFieldValue) {
+        inputField.addClass('empty-field').attr('placeholder', 'Please enter a new task');
+    } else {
+        inputField.removeClass('empty-field').val('');
+        $('.task-list').append(listItem);
+    }
+});
