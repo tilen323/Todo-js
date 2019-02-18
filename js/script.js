79ed7ced4f3d0da3ccc78c14a612ec5a9d45208d
Tilen Prevolnik <tilen.prevolnik@gigodesign.com>
2019-02-18 09:35:33 +0100

    change inputField value to text-string

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 $('.input-box--submit').on('click', function () {
     let inputField = $('.input-box--input');
     let inputFieldValue = inputField.val();
-    let listItem = '<li class="task-list--item">' + inputFieldValue + '</li>';
+    let listItem = $('<li class="task-list--item" />');
 
     if (!inputFieldValue) {
         inputField.addClass('empty-field').attr('placeholder', 'Please enter a new task');
     } else {
         inputField.removeClass('empty-field').val('').attr('placeholder', 'Enter new task');
-        $('.task-list').append(listItem);
+        $('.task-list').append(listItem.text(inputFieldValue));
     }
 });
